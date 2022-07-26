f7867fe187c0ce325ca33b430c4079bcebdfce55
Paloma Gareis Borgiani <palomagareisborgiani@gmail.com>
2022-07-26 05:30:12 -0300

    subiendo API

---
+++ BackEnd/src/main/java/com/example/portfolioapi/repos/LaboralRepo.java
@@ -0,0 +1,16 @@
+
+
+package com.example.portfolioapi.repos;
+
+import com.example.portfolioapi.models.LaboralModel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LaboralRepo extends CrudRepository<LaboralModel, Integer> {
+
+    //public Optional<LaboralModel> findById(int id);
+
+    //public void deleteById(long id);
+    
+}
